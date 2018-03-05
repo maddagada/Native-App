a8f6d326bae69716e7df25de4a84b03278b56fce
maddagada <maddagada@users.noreply.github.com>
2018-03-04 19:41:48 -0600

    Initial commit test app

---
+++ app/src/main/java/com/vogella/android/testapp/ConverterUtil.java
@@ -0,0 +1,18 @@
+package com.vogella.android.testapp;
+
+/**
+ * Created by IT_Admin on 3/4/2018.
+ */
+
+public class ConverterUtil {
+    // converts to celsius
+    public static float convertFahrenheitToCelsius(float fahrenheit) {
+        return ((fahrenheit - 32) * 5 / 9);
+    }
+
+    // converts to fahrenheit
+    public static float convertCelsiusToFahrenheit(float celsius) {
+        return ((celsius * 9) / 5) + 32;
+    }
+
+}
